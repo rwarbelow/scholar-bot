270acd98c8f46e3a5b6e6c7b5e74efcc8d75813b
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-09-15 14:10:23 -0700

    add debugging code

--- app/controllers/admin/guardians_controller.rb
+++ app/controllers/admin/guardians_controller.rb
@@ -17,12 +17,18 @@ class Admin::GuardiansController < Admin::BaseController
 	end
 
 	def create
+		p "in create"
 		student_ids = params[:student_ids]
+		p student_ids
 		@guardian = Guardian.new(guardian_params)
+		p "new guardian"
 		@guardian.generate_code
+		p "generated code"
 		@guardian.password = @guardian.code
 		@guardian.password_confirmation = @guardian.code
+		p "assigned code as password"
 		if @guardian.save
+			p "saved guardian"
 			@guardian.create_guardianships(student_ids)
 			redirect_to admin_guardians_path, notice: 'Guardian was successfully created.'
 		else
