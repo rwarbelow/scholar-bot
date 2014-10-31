fbd7aa56f77de455b7f53d60e041ca8d338c16fa
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-10-30 22:05:33 -0600

    remove pry

--- app/controllers/teachers/reports_controller.rb
+++ app/controllers/teachers/reports_controller.rb
@@ -1,10 +1,7 @@
 class Teachers::ReportsController < Teachers::BaseController
 	def index
-		p params
 		start = params[:start].to_i
 		stop = params[:stop].to_i
-		require 'pry'
-		binding.pry
 		@students = Student.where(:id => start..stop)
 		@core_values = CoreValue.all
 	end
